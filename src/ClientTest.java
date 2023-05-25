9d817a9a90fd8eaeecf6f3c0457ab02c0449e3c5
murtazintagir <tr.murtazin@yandex.ru>
2023-05-25 13:51:11 +0300

    v0.7 Доработаны регулярные выражения для проверки IP, email, адреса проживания и адреса URL.

--- src/ClientTest.java
+++ src/ClientTest.java
@@ -11,13 +11,24 @@ public class ClientTest {
 
     public static void main(String[] args) {
         Client client1 = new Client("Клиент 1",
-                "tr.murtazin@yandex.ru", // адрес электронной почты
-                "255.255.255.255",      // ip-адрес
-                "   РФ, @!#г.Санкт-Петербург,     Ленинский проспект, 57/2",                     // адрес проживания
-                "https://vk.com/im?sel=c46");      // URL-адрес
-        System.out.println(client1.getAddress());
+                "tr.murtazin@yandex.ru",                                 // адрес электронной почты
+                "255.255.255.255",                                             // ip-адрес
+                "РФ, @!#г.Санкт-Петербург,     ул.Ленинский проспект, 57",   // адрес проживания
+                "https://github.com/murtazintagir");                           // URL адрес
         clientValidator(client1);
-        System.out.println(client1.getAddress());
 
+        Client client2 = new Client("Клиент 2",
+                "@.es",                                 // адрес электронной почты
+                "727.0.0.255",                                             // ip-адрес
+                "Испания, г.Барселона, ул.Майорка, 401",   // адрес проживания
+                "https://sagradafamilia.org/en/?lang=");                           // URL адрес
+        clientValidator(client2);
+
+        Client client3 = new Client("Клиент 3",
+                "vel_luk@.es",                                 // адрес электронной почты
+                "127.0.0.255",                                             // ip-адрес
+                "РФ, г.Велики Луки, ул.Пушкина, 3/",   // адрес проживания
+                "");                           // URL адрес
+        clientValidator(client3);
     }
 }
--- src/ClientValidator.java
+++ src/ClientValidator.java
@@ -3,52 +3,57 @@ import java.util.regex.Pattern;
 class ClientValidator {
     public void validateEmail(Client client) {
         boolean result;
-        String regex = "^([a-zA-Z]+[\\w.-]+@[a-zA-Z]+[\\w.-]+\\.(com|ru|net|org))$";
-        result = Pattern.matches(regex, client.getEmail());
+        String regex1 = "^[a-zA-Z]+[\\w.-]+";
+        String regex2 = "@[a-zA-Z]+[\\w.-]+.(com|ru|net|org)$";
+        result = Pattern.matches(regex1 + regex2, client.getEmail());
         if (result) {
-            System.out.println("У клиента " + client.getName() + " адрес электронной почты корректен");
+            System.out.println("У пользователя " + client.getName() + " адрес электронной почты корректен");
         } else {
-            System.out.println("Адрес электронной почты некорректен");
+            System.out.println("У пользователя " + client.getName() + " адрес электронной почты некорректен");
         }
     }
 
     public void validateIP(Client client) {
         boolean result;
-        String regex = "^(((\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.){3}(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5]))$";
-        result = Pattern.matches(regex, client.getIp());
+        String regex1 = "^(((\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])\\.){3}";
+        String regex2 = "(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5]))$";
+        result = Pattern.matches(regex1 + regex2, client.getIp());
         if (result) {
-            System.out.println("IP-адрес корректен");
+            System.out.println("У пользователя " + client.getName() + " IP-адрес корректен");
         } else {
-            System.out.println("IP-адрес некорректен");
+            System.out.println("У пользователя " + client.getName() + " IP-адрес некорректен");
         }
     }
 
     public void validateUrl(Client client) {
         boolean result;
-        // https://vk.com/im?sel=c46
-        String regex = "^((http|https):/{2})?(([a-zA-Z].)+|\\w+.)(com|ru|)/?\\w+/*$";//+(/{2}|\\.[a-z]+)$";
-        result = Pattern.matches(regex, client.getUrl());
+        String regex1 = "^((http|https):/{2})?";
+        String regex2 = "(([\\w-]+[.])+([a-z]{2,6})/?|" +
+                "w{3}.([\\w-]+[.])+[a-z]{2,6}/?)";
+        String regex3 = "(([\\w-.]+/?)+|(([?]?[\\w-=.]+)/?)+)$";
+        result = Pattern.matches(regex1 + regex2 + regex3, client.getUrl());
         if (result) {
-            System.out.println("Адрес URL корректен");
+            System.out.println("У пользователя " + client.getName() + " адрес URL корректен");
         } else {
-            System.out.println("Адрес URL некорректен");
+            System.out.println("У пользователя " + client.getName() + " адрес URL некорректен");
         }
     }
 
     public void validateAddress(Client client) {
         boolean result;
-        // ^(([А-Я][а-я]+( [А-Я][а-я]+)*)|[А-Я]{2,}), (г.[А-Я][а-я]+[ -][А-Я][а-я]*)$
-        String regex = "^(([А-Я][а-я]+( [А-Я][а-я]+)*)|[А-Я]{2,}), (г.[А-Я][а-я]+[ -][А-Я][а-я]*)," +
-                " ([ул.]?[А-Я]?[а-я]+[ -][А-Я]?[а-я]+), (\\d+(/\\d+)*)$";
-        String address = client.getAddress().replaceAll("[%$@<>?*()^#!]+", " ")
+        String regex1 = "^(([А-Я][а-я]+( [А-Я][а-я]+)*)|[А-Я]{2,}),"; // РВ для проверки страны
+        String regex2 = " (г.[А-Я][а-я]+([ -][А-Я][а-я]+)*),";           // РВ для проверки города
+        String regex3 = " ((ул.)?[А-Я]?[а-я]+([ -][А-Я]?[а-я]+)*), (\\d+(/\\d+)*)$"; // РВ для проверки улицы и дома
+        // Удаляем спец. символы и лишние пробелы
+        String address = client.getAddress().replaceAll("[%$@<>?*()^#!{}]+", " ")
                 .replaceAll(" {2,}", " ")
                 .trim();
-        result = Pattern.matches(regex, address);
+        result = Pattern.matches(regex1 + regex2 + regex3, address);
         if (result) {
-            System.out.println("Адрес проживания корректен");
-            client.setAddress(address);
+            System.out.println("У пользователя " + client.getName() + " адрес проживания корректен");
+            client.setAddress(address); // записываем адрес после удаления лишних символов
         } else {
-            System.out.println("Адрес проживания некорректен");
+            System.out.println("У пользователя " + client.getName() + " адрес проживания некорректен");
         }
     }
 }
